53bfd7d8dbfce19fd03e9c4c06c3082d04e4f721
Steven Ha <109867418+stevenha75@users.noreply.github.com>
2024-04-19 22:03:27 -0400

    Clearing app.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,15 @@ import App from './App'
 import './index.css'
 
 import './demos/ipc'
+import { BrowserRouter as Router } from 'react-router-dom';
 // If you want use Node.js, the`nodeIntegration` needs to be enabled in the Main process.
 // import './demos/node'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
 )
 
